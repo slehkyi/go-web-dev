829d73bb6f56fe15d0e3bee0699cdf375a3bcfc2
slehkyi <s.lehkyi@cronostelecom.com>
2017-05-18 18:38:49 +0200

    new lessons

---
+++ 17-understanding_net_http_package/04-ResponseWriter/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("New-Key","New Value")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintln(w, "<h1>Any code you want</h1>")
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
