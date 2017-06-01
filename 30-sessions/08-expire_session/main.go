f5b5c262b309e0e682151198cbd3d843f32f4aa9
slehkyi <s.lehkyi@cronostelecom.com>
2017-06-01 12:32:32 +0200

    sessions

---
+++ 30-sessions/08-expire_session/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type user struct {
+	UserName string
+	Password []byte
+	First string
+	Last string
+	Role string
+}
+
+type session struct {
+	un string
+	lastActivity time.Time
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{} // userID, user{}
+var dbSessions = map[string]session{} // sessionID, session
+var dbSessionsCleaned time.Time
+const sessionLength int = 30
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+	dbSessionsCleaned = time.Now()
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	u := getUser(w, r)
+	showSessions()
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	u := getUser(w, r)
+	if !alreadyLoggedIn(w, r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	if u.Role != "007" {
+		http.Error(w, "You must be 007 to enter the bar", http.StatusForbidden)
+		return
+	}
+	showSessions()
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	if alreadyLoggedIn(w, r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	//process form submission
+	if r.Method == http.MethodPost {
+		// get form values
+		un := r.FormValue("username")
+		p := r.FormValue("password")
+		f := r.FormValue("firstname")
+		l := r.FormValue("lastname")
+		role := r.FormValue("role")
+
+		//username taken?
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already exists", http.StatusForbidden)
+			return
+		}
+
+		//create session
+		sID := uuid.NewV4()
+		c := &http.Cookie{
+			Name:"session",
+			Value:sID.String(),
+		}
+		c.MaxAge = sessionLength
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = session{un, time.Now()}
+
+		//store user in dbUsers
+		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		u := user{un, bs, f, l, role}
+		dbUsers[un] = u
+
+		//redirect
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	showSessions()
+	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if alreadyLoggedIn(w, r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	// process form submission
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username")
+		p := r.FormValue("password")
+		// is there a username?
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(w, "User doesn't exist", http.StatusForbidden)
+			return
+		}
+		// does the entered password match the stored one?
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "Password does not match", http.StatusForbidden)
+			return
+		}
+		// create session
+		sID := uuid.NewV4()
+		c := &http.Cookie{
+			Name: "session",
+			Value: sID.String(),
+		}
+		c.MaxAge = sessionLength
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = session{un, time.Now()}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	showSessions()
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	if !alreadyLoggedIn(w, r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	c, _ := r.Cookie("session")
+	// delete session
+	delete(dbSessions, c.Value)
+	// remove cookie
+	c = &http.Cookie{
+		Name: "session",
+		Value: "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, c)
+
+	// clean up dbSessions
+	if time.Now().Sub(dbSessionsCleaned) > (time.Second * 30) {
+		go cleanSessions()
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
